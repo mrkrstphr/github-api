698b109cf51b62faf88828b9930826aa9e98ab4e
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-08 19:04:29 -0400

    repositories implementation

---
+++ src/Martha/GitHub/Request/MalformedRequestException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Martha\GitHub\Request;
+
+/**
+ * Class MalformedRequestException
+ * @package Martha\GitHub\Request
+ */
+class MalformedRequestException extends \Exception
+{
+}
