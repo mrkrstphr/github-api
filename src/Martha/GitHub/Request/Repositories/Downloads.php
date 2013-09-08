38d6a98224a54f841c41e8567f346b5d6714848d
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-08 19:42:56 -0400

    stubbing Downloads end point

--- src/Martha/GitHub/Request/Repositories/Downloads.php
+++ src/Martha/GitHub/Request/Repositories/Downloads.php
@@ -12,4 +12,49 @@ use Martha\GitHub\Request\AbstractRequest;
  */
 class Downloads extends AbstractRequest
 {
+    /**
+     * @see http://developer.github.com/v3/repos/downloads/#list-downloads-for-a-repository
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     */
+    public function downloads($owner, $repo)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/downloads/#get-a-single-download
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     * @return array
+     */
+    public function download($owner, $repo, $id)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/downloads/#create-a-new-download-part-1-create-the-resource
+     * @param string $owner
+     * @param string $repo
+     * @param array $parameters
+     * @return array
+     */
+    public function create($owner, $repo, array $parameters)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/downloads/#delete-a-download
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     */
+    public function delete($owner, $repo, $id)
+    {
+
+    }
 }
