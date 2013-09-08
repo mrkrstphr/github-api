7d2d777064ba5ddc8476908c5517907b65f0044c
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-08 19:36:02 -0400

    stubbing out Commits

--- src/Martha/GitHub/Request/Repositories/Commits.php
+++ src/Martha/GitHub/Request/Repositories/Commits.php
@@ -12,4 +12,40 @@ use Martha\GitHub\Request\AbstractRequest;
  */
 class Commits extends AbstractRequest
 {
+    /**
+     * @see http://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
+     * @param string $user
+     * @param string $repo
+     * @param array $parameters
+     * @return array
+     */
+    public function commits($user, $repo, array $parameters = array())
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/commits/#get-a-single-commit
+     * @param string $user
+     * @param string $repo
+     * @param $sha
+     * @return array
+     */
+    public function commit($user, $repo, $sha)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/commits/#compare-two-commits
+     * @param string $user
+     * @param string $repo
+     * @param string $base
+     * @param string $head
+     * @return array
+     */
+    public function compare($user, $repo, $base, $head)
+    {
+        return array();
+    }
 }
