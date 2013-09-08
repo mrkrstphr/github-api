f9bc57853ea76f6b921a7399ce54cee41344e493
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-08 19:46:57 -0400

    stubbing Forks end point

--- src/Martha/GitHub/Request/Repositories/Forks.php
+++ src/Martha/GitHub/Request/Repositories/Forks.php
@@ -12,4 +12,27 @@ use Martha\GitHub\Request\AbstractRequest;
  */
 class Forks extends AbstractRequest
 {
+    /**
+     * @see http://developer.github.com/v3/repos/forks/#list-forks
+     * @param string $owner
+     * @param string $repo
+     * @param array $parameters
+     * @return array
+     */
+    public function forks($owner, $repo, array $parameters = array())
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/forks/#create-a-fork
+     * @param string $owner
+     * @param string $repo
+     * @param array $parameters
+     * @return array
+     */
+    public function create ($owner, $repo, array $parameters = array())
+    {
+        return array();
+    }
 }
