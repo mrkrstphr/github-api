b7b859ff1d72d5157f03b116ef0c95778e320cc1
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-08 19:47:02 -0400

    stubbing Keys end point

--- src/Martha/GitHub/Request/Repositories/Keys.php
+++ src/Martha/GitHub/Request/Repositories/Keys.php
@@ -10,4 +10,60 @@ namespace Martha\GitHub\Request\Repositories;
  */
 class Keys
 {
+    /**
+     * @see http://developer.github.com/v3/repos/keys/#list
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     */
+    public function keys($owner, $repo)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/keys/#get
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     * @return array
+     */
+    public function key($owner, $repo, $id)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/keys/#create
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     */
+    public function create($owner, $repo)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/keys/#edit
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     * @return array
+     */
+    public function edit($owner, $repo, $id)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/keys/#delete
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     */
+    public function delete($owner, $repo, $id)
+    {
+
+    }
 }
