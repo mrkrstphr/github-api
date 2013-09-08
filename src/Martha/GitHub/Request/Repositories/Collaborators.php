ee4ee954b09a3130ea444eac46d69194aa5f26bc
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-08 19:33:57 -0400

    stubbing out collaborators and comments

--- src/Martha/GitHub/Request/Repositories/Collaborators.php
+++ src/Martha/GitHub/Request/Repositories/Collaborators.php
@@ -12,4 +12,48 @@ use Martha\GitHub\Request\AbstractRequest;
  */
 class Collaborators extends AbstractRequest
 {
+    /**
+     * @see http://developer.github.com/v3/repos/collaborators/#list
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     */
+    public function collaborators($owner, $repo)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/collaborators/#get
+     * @param string $owner
+     * @param string $repo
+     * @param string $user
+     * @return array
+     */
+    public function get($owner, $repo, $user)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/collaborators/#add-collaborator
+     * @param string $owner
+     * @param string $repo
+     * @param string $user
+     * @return array
+     */
+    public function add($owner, $repo, $user)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/collaborators/#remove-collaborator
+     * @param string $owner
+     * @param string $repo
+     * @param string $user
+     */
+    public function remove($owner, $repo, $user)
+    {
+    }
 }
--- src/Martha/GitHub/Request/Repositories/Comments.php
+++ src/Martha/GitHub/Request/Repositories/Comments.php
@@ -12,4 +12,75 @@ use Martha\GitHub\Request\AbstractRequest;
  */
 class Comments extends AbstractRequest
 {
+    /**
+     * @see http://developer.github.com/v3/repos/comments/#list-commit-comments-for-a-repository
+     * @param string $user
+     * @param string $repo
+     * @return array
+     */
+    public function comments($user, $repo)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/comments/#list-comments-for-a-single-commit
+     * @param string $user
+     * @param string $repo
+     * @param string $sha
+     * @return array
+     */
+    public function commitComments($user, $repo, $sha)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/comments/#create-a-commit-comment
+     * @param string $user
+     * @param string $repo
+     * @param string $sha
+     * @param array $parameters
+     * @return array
+     */
+    public function create($user, $repo, $sha, array $parameters)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/comments/#get-a-single-commit-comment
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     * @return array
+     */
+    public function get($owner, $repo, $id)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/comments/#update-a-commit-comment
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     * @param array $parameters
+     * @return array
+     */
+    public function update($owner, $repo, $id, array $parameters)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/comments/#delete-a-commit-comment
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     */
+    public function delete($owner, $repo, $id)
+    {
+
+    }
 }
