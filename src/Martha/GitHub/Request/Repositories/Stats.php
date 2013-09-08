07e2cf37c477ef342d4947076a01cb098de33066
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-08 19:52:27 -0400

    stubbing Stats end point

--- src/Martha/GitHub/Request/Repositories/Stats.php
+++ src/Martha/GitHub/Request/Repositories/Stats.php
@@ -12,4 +12,58 @@ use Martha\GitHub\Request\AbstractRequest;
  */
 class Stats extends AbstractRequest
 {
+    /**
+     * @see http://developer.github.com/v3/repos/statistics/#get-contributors-list-with-additions-deletions-and-commit-counts
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     */
+    public function contributors($owner, $repo)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/statistics/#get-the-last-year-of-commit-activity-data
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     */
+    public function commitActivity($owner, $repo)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/statistics/#get-the-number-of-additions-and-deletions-per-week
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     */
+    public function codeFrequency($owner, $repo)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/statistics/#get-the-weekly-commit-count-for-the-repo-owner-and-everyone-else
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     */
+    public function participation($owner, $repo)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/statistics/#get-the-number-of-commits-per-hour-in-each-day
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     */
+    public function punchChard($owner, $repo)
+    {
+        return array();
+    }
 }
