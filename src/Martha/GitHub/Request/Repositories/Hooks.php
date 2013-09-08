fc2066cf921f8a60a998e0da3310e670d37c805f
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-08 19:55:36 -0400

    stubbing Hook end point

--- src/Martha/GitHub/Request/Repositories/Hooks.php
+++ src/Martha/GitHub/Request/Repositories/Hooks.php
@@ -12,4 +12,73 @@ use Martha\GitHub\Request\AbstractRequest;
  */
 class Hooks extends AbstractRequest
 {
+    /**
+     * @see http://developer.github.com/v3/repos/hooks/#list
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     */
+    public function hooks($owner, $repo)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/hooks/#get-single-hook
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     * @return array
+     */
+    public function hook($owner, $repo, $id)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/hooks/#create-a-hook
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     */
+    public function create($owner, $repo)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/hooks/#edit-a-hook
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     * @return array
+     */
+    public function edit($owner, $repo, $id)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/hooks/#test-a-push-hook
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     * @return array
+     */
+    public function test($owner, $repo, $id)
+    {
+        return array();
+    }
+
+    /**
+     * @see http://developer.github.com/v3/repos/hooks/#delete-a-hook
+     * @param string $owner
+     * @param string $repo
+     * @param string $id
+     * @return array
+     */
+    public function delete($owner, $repo, $id)
+    {
+        return array();
+    }
 }
