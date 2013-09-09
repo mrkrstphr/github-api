1238ea9f8687777502ad74e9b7f150dcf6136bf9
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-09 13:07:17 -0400

    Finished Repositories\Content implementation

--- src/Martha/GitHub/Request/Repositories/Contents.php
+++ src/Martha/GitHub/Request/Repositories/Contents.php
@@ -3,6 +3,7 @@
 namespace Martha\GitHub\Request\Repositories;
 
 use Martha\GitHub\Request\AbstractRequest;
+use Martha\GitHub\Request\MalformedRequestException;
 
 /**
  * Class Contents
@@ -13,6 +14,8 @@ use Martha\GitHub\Request\AbstractRequest;
 class Contents extends AbstractRequest
 {
     /**
+     * Get the preferred readme for a given repository.
+     *
      * @see http://developer.github.com/v3/repos/contents/#get-the-readme
      * @param string $owner
      * @param string $repo
@@ -28,6 +31,8 @@ class Contents extends AbstractRequest
     }
 
     /**
+     * Get the contents of a file from a given repository.
+     *
      * @see http://developer.github.com/v3/repos/contents/#get-contents
      * @param string $owner
      * @param string $repo
@@ -44,8 +49,10 @@ class Contents extends AbstractRequest
     }
 
     /**
-     * @todo
+     * Create and commit a file. The content specified in $parameters must be base64 encoded.
+     *
      * @see http://developer.github.com/v3/repos/contents/#create-a-file
+     * @throws MalformedRequestException
      * @param string $owner
      * @param string $repo
      * @param string $path
@@ -54,12 +61,25 @@ class Contents extends AbstractRequest
      */
     public function create($owner, $repo, $path, array $parameters)
     {
-        return array();
+        if (!isset($parameters['message'])) {
+            throw new MalformedRequestException('A commit message is required to create a file');
+        }
+
+        if (!isset($parameters['content'])) {
+            throw new MalformedRequestException('Content is required to create a file');
+        }
+
+        return $this->client->put(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/contents/' . urlencode($path),
+            $parameters
+        );
     }
 
     /**
-     * @todo
+     * Updates and commits the specified file. The content specified in $parameters must be base64 encoded.
+     *
      * @see http://developer.github.com/v3/repos/contents/#update-a-file
+     * @throws MalformedRequestException
      * @param string $owner
      * @param string $repo
      * @param string $path
@@ -68,12 +88,29 @@ class Contents extends AbstractRequest
      */
     public function update($owner, $repo, $path, array $parameters)
     {
-        return array();
+        if (!isset($parameters['message'])) {
+            throw new MalformedRequestException('A commit message is required to create a file');
+        }
+
+        if (!isset($parameters['sha'])) {
+            throw new MalformedRequestException('SHA hash of the file being updated is required');
+        }
+
+        if (!isset($parameters['content'])) {
+            throw new MalformedRequestException('Content is required to create a file');
+        }
+
+        return $this->client->put(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/contents/' . urlencode($path),
+            $parameters
+        );
     }
 
     /**
-     * @todo
+     * Deletes a file from a repository.
+     *
      * @see http://developer.github.com/v3/repos/contents/#delete-a-file
+     * @throws MalformedRequestException
      * @param string $owner
      * @param string $repo
      * @param string $path
@@ -82,11 +119,23 @@ class Contents extends AbstractRequest
      */
     public function delete($owner, $repo, $path, array $parameters)
     {
-        return array();
+        if (!isset($parameters['message'])) {
+            throw new MalformedRequestException('Message is required to delete a file');
+        }
+
+        if (!isset($parameters['sha'])) {
+            throw new MalformedRequestException('SHA hash of the file being removed is required');
+        }
+
+        return $this->client->delete(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/contents/' . urlencode($path),
+            $parameters
+        );
     }
 
     /**
-     * @todo
+     * Download an archive of the repository. Returns the archive file contents in the requested format.
+     *
      * @see http://developer.github.com/v3/repos/contents/#get-archive-link
      * @param string $owner
      * @param string $repo
@@ -97,6 +146,11 @@ class Contents extends AbstractRequest
      */
     public function archive($owner, $repo, $format, $ref, array $parameters = array())
     {
-        return '';
+        $response = $this->client->get(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/' . urlencode($format) . '/' . urlencode($ref),
+            $parameters
+        );
+
+        return $response;
     }
 }
