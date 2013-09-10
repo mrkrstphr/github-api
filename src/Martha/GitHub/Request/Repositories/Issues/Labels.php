13df7f6ca7b21e98d3927eccbafc5f780de482cc
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-10 12:47:29 -0400

    implementing labels api end point

--- src/Martha/GitHub/Request/Repositories/Issues/Labels.php
+++ src/Martha/GitHub/Request/Repositories/Issues/Labels.php
@@ -15,7 +15,6 @@ class Labels extends AbstractRequest
     /**
      * List labels on an issue.
      *
-     * @todo
      * @see http://developer.github.com/v3/issues/labels/#list-labels-on-an-issue
      * @param string $owner
      * @param string $repo
@@ -24,13 +23,14 @@ class Labels extends AbstractRequest
      */
     public function labels($owner, $repo, $number)
     {
-        return array();
+        return $this->client->get(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/issues/' . urlencode($number) . '/labels'
+        );
     }
 
     /**
      * Add labels to an issue.
      *
-     * @todo
      * @see http://developer.github.com/v3/issues/labels/#add-labels-to-an-issue
      * @param string $owner
      * @param string $repo
@@ -40,13 +40,15 @@ class Labels extends AbstractRequest
      */
     public function create($owner, $repo, $number, array $parameters)
     {
-        return array();
+        return $this->client->post(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/issues/' . urlencode($number) . '/labels',
+            $parameters
+        );
     }
 
     /**
      * If $name is specified, remove that label from the issue, otherwise remove all labels from the issue.
      *
-     * @todo
      * @see http://developer.github.com/v3/issues/labels/#remove-a-label-from-an-issue
      * @see http://developer.github.com/v3/issues/labels/#remove-all-labels-from-an-issue
      * @param string $owner
@@ -56,13 +58,18 @@ class Labels extends AbstractRequest
      */
     public function delete($owner, $repo, $number, $name = '')
     {
+        $url = '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/issues/' . urlencode($number) . '/labels';
 
+        if ($name) {
+            $url .= '/' . urlencode($name);
+        }
+
+        $this->client->delete($url);
     }
 
     /**
      * Replace all labels for an issue.
      *
-     * @todo
      * @see http://developer.github.com/v3/issues/labels/#replace-all-labels-for-an-issue
      * @param string $owner
      * @param string $repo
@@ -70,8 +77,11 @@ class Labels extends AbstractRequest
      * @param array $parameters
      * @return array
      */
-    public function replace($owner, $repo, $number, array $parameters)
+    public function replace($owner, $repo, $number, array $parameters = array())
     {
-        return array();
+        return $this->client->put(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/issues/' . urlencode($number) . '/labels',
+            $parameters
+        );
     }
 }
