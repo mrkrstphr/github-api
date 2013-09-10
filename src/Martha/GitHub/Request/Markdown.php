33b83150ff48186d99ce44c203f1a476bfa4345f
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-09 23:53:29 -0400

    Implementing Markdown API calls

--- src/Martha/GitHub/Request/Markdown.php
+++ src/Martha/GitHub/Request/Markdown.php
@@ -8,4 +8,32 @@ namespace Martha\GitHub\Request;
  */
 class Markdown extends AbstractRequest
 {
+    /**
+     * Render an arbitrary markdown document.
+     *
+     * @see http://developer.github.com/v3/markdown/#render-an-arbitrary-markdown-document
+     * @param array $parameters
+     * @return array
+     * @throws MalformedRequestException
+     */
+    public function markdown(array $parameters)
+    {
+        if (!isset($parameters['text'])) {
+            throw new MalformedRequestException('Text is required to render');
+        }
+
+        return $this->client->post('/markdown', $parameters);
+    }
+
+    /**
+     * Render a Markdown document in raw mode.
+     *
+     * @see http://developer.github.com/v3/markdown/#render-a-markdown-document-in-raw-mode
+     * @param string $contents
+     * @return array
+     */
+    public function raw($contents)
+    {
+        return $this->client->post('/markdown/raw', $contents);
+    }
 }
