6631095570b56617997d1f7bfba3d5ead0002645
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-12 18:48:27 -0400

    organization team management

--- src/Martha/GitHub/Request/Organizations/Teams.php
+++ src/Martha/GitHub/Request/Organizations/Teams.php
@@ -3,6 +3,7 @@
 namespace Martha\GitHub\Request\Organizations;
 
 use Martha\GitHub\Request\AbstractRequest;
+use Martha\GitHub\Request\MalformedRequestException;
 
 /**
  * Class Teams
@@ -12,18 +13,33 @@ use Martha\GitHub\Request\AbstractRequest;
  */
 class Teams extends AbstractRequest
 {
+    /**
+     * List teams.
+     *
+     * @see http://developer.github.com/v3/orgs/teams/#list-teams
+     * @param string $organization
+     * @return array
+     */
     public function teams($organization)
     {
-        return array();
+        return $this->getClient()->get('/orgs/' . urlencode($organization) . '/teams');
     }
 
-    public function team($id)
-    {
-        return array();
-    }
-
+    /**
+     * Create a team.
+     *
+     * @see http://developer.github.com/v3/orgs/teams/#create-team
+     * @throws MalformedRequestException
+     * @param $organization
+     * @param array $parameters
+     * @return array
+     */
     public function create($organization, array $parameters)
     {
-        return array();
+        if (!isset($parameters['name'])) {
+            throw new MalformedRequestException('Name is required to create a team');
+        }
+
+        return $this->getClient()->post('/orgs/' . urlencode($organization) . '/teams', $parameters);
     }
 }
