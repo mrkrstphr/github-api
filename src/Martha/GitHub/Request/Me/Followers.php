2bb4bc196857b0a660fc3b81c0ac876e8f30340d
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-12 00:00:13 -0400

    implementing Me/Followers API

--- src/Martha/GitHub/Request/Me/Followers.php
+++ src/Martha/GitHub/Request/Me/Followers.php
@@ -15,61 +15,59 @@ class Followers extends AbstractRequest
     /**
      * List the authenticated user's followers.
      *
-     * @todo
      * @see http://developer.github.com/v3/users/followers/#list-followers-of-a-user
      * @return array
      */
     public function followers()
     {
-        return array();
+        return $this->getClient()->get('/user/followers');
     }
 
     /**
      * List who the authenticated user is following.
      *
-     * @todo
      * @see http://developer.github.com/v3/users/followers/#list-users-followed-by-another-user
      * @return array
      */
     public function following()
     {
-        return array();
+        return $this->getClient()->get('/user/following');
     }
 
     /**
      * Check if the authenticated user is following a user.
      *
-     * @todo
      * @see http://developer.github.com/v3/users/followers/#check-if-you-are-following-a-user
      * @param $user
      * @return bool
      */
     public function amFollowing($user)
     {
-        return true;
+        $this->getClient()->get('/user/following/' . urlencode($user));
+        $response = $this->getClient()->getLastResponse();
+
+        return $response->getStatusCode() == '204';
     }
 
     /**
      * Follow a user.
      *
-     * @todo
      * @see http://developer.github.com/v3/users/followers/#follow-a-user
      * @param string $user
      */
     public function follow($user)
     {
-
+        $this->getClient()->put('/user/following/' . urlencode($user));
     }
 
     /**
      * Unfollow a user.
      *
-     * @todo
      * @see http://developer.github.com/v3/users/followers/#unfollow-a-user
      * @param string $user
      */
     public function unfollow($user)
     {
-
+        $this->getClient()->delete('/user/following/' . urlencode($user));
     }
 }
