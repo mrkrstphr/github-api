187c97f45a5fb749b58dde669aebccb3c6c96f71
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-11 23:35:07 -0400

    implementing pulls api

--- src/Martha/GitHub/Request/Repositories/Pulls.php
+++ src/Martha/GitHub/Request/Repositories/Pulls.php
@@ -3,9 +3,12 @@
 namespace Martha\GitHub\Request\Repositories;
 
 use Martha\GitHub\Request\AbstractRequest;
+use Martha\GitHub\Request\MalformedRequestException;
 
 /**
  * Class PullRequests
+ *
+ * @see http://developer.github.com/v3/pulls/
  * @package Martha\GitHub\Request
  */
 class Pulls extends AbstractRequest
@@ -13,7 +16,6 @@ class Pulls extends AbstractRequest
     /**
      * List pull requests.
      *
-     * @todo
      * @see http://developer.github.com/v3/pulls/#list-pull-requests
      * @param string $owner
      * @param string $repo
@@ -22,13 +24,15 @@ class Pulls extends AbstractRequest
      */
     public function pulls($owner, $repo, array $parameters = array())
     {
-        return array();
+        return $this->getClient()->get(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo),
+            $parameters
+        );
     }
 
     /**
      * Get a single pull request.
      *
-     * @todo
      * @see http://developer.github.com/v3/pulls/#get-a-single-pull-request
      * @param string $owner
      * @param string $repo
@@ -37,14 +41,16 @@ class Pulls extends AbstractRequest
      */
     public function pull($owner, $repo, $number)
     {
-        return array();
+        return $this->getClient()->get(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/pulls/' . urlencode($number)
+        );
     }
 
     /**
      * Create a pull request.
      *
-     * @todo
      * @see http://developer.github.com/v3/pulls/#create-a-pull-request
+     * @throws MalformedRequestException
      * @param $owner
      * @param $repo
      * @param array $parameters
@@ -52,28 +58,41 @@ class Pulls extends AbstractRequest
      */
     public function create($owner, $repo, array $parameters)
     {
-        return array();
+        if (!isset($parameters['title'])) {
+            throw new MalformedRequestException('Title is required to create a pull request');
+        }
+
+        if (!isset($parameters['base']) || !isset($parameters['head'])) {
+            throw new MalformedRequestException('Base and Head are required to create a pull request');
+        }
+
+        return $this->getClient()->post(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/pulls',
+            $parameters
+        );
     }
 
     /**
      * Update a pull request.
      *
-     * @todo
      * @see http://developer.github.com/v3/pulls/#update-a-pull-request
      * @param $owner
      * @param $repo
+     * @param $number
      * @param array $parameters
      * @return array
      */
-    public function update($owner, $repo, array $parameters)
+    public function update($owner, $repo, $number, array $parameters)
     {
-        return array();
+        return $this->getClient()->patch(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/pulls/' . urlencode($number),
+            $parameters
+        );
     }
 
     /**
      * List commits on a pull request.
      *
-     * @todo
      * @see http://developer.github.com/v3/pulls/#list-commits-on-a-pull-request
      * @param string $owner
      * @param string $repo
@@ -82,13 +101,14 @@ class Pulls extends AbstractRequest
      */
     public function commits($owner, $repo, $number)
     {
-        return array();
+        return $this->getClient()->get(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/pulls/' . urlencode($number) . '/commits'
+        );
     }
 
     /**
      * List pull request files
      *
-     * @todo
      * @see http://developer.github.com/v3/pulls/#list-pull-requests-files
      * @param string $owner
      * @param string $repo
@@ -97,13 +117,14 @@ class Pulls extends AbstractRequest
      */
     public function files($owner, $repo, $number)
     {
-        return array();
+        return $this->getClient()->get(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/pulls/' . urlencode($number) . '/files'
+        );
     }
 
     /**
      * Get if a pull request has been merged
      *
-     * @todo
      * @see http://developer.github.com/v3/pulls/#get-if-a-pull-request-has-been-merged
      * @param string $owner
      * @param string $repo
@@ -112,22 +133,31 @@ class Pulls extends AbstractRequest
      */
     public function wasMerged($owner, $repo, $number)
     {
-        return true;
+        $this->getClient()->get(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/pulls/' . urlencode($number) . '/merge'
+        );
+
+        $response = $this->getClient()->getLastResponse();
+
+        return $response->getStatusCode() == '204';
     }
 
     /**
      * Merge a pull request.
      *
-     * @todo
      * @see http://developer.github.com/v3/pulls/#merge-a-pull-request-merge-buttontrade
      * @param string $owner
      * @param string $repo
      * @param string $number
+     * @param array $parameters
      * @return array
      */
-    public function merge($owner, $repo, $number)
+    public function merge($owner, $repo, $number, array $parameters = array())
     {
-        return array();
+        return $this->getClient()->put(
+            '/repos/' . urlencode($owner) . '/' . urlencode($repo) . '/pulls/' . urlencode($number) . '/merge',
+            $parameters
+        );
     }
 
     /**
